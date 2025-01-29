9f43a6289165bf0ff945eaaeb3b87ea2146e2d2b
Daniel Oliveira <danieldfs18@hotmail.com>
2025-01-29 16:23:57 -0300

    Add new test scenario

--- test/expertRecency.test.mjs
+++ test/expertRecency.test.mjs
@@ -79,40 +79,22 @@ describe('Expert Recency API Tests', () => {
   });
 
 
-  // it('Should validate filtering from now to 6 months', async () => {
-  //   const res = await request(baseUrl)
-  //     .post(endpoint)
-  //     .send({
-  //       filters: {
-  //         status: [],
-  //         groups: [],
-  //         recency: ["<6"]
-  //       }
-  //     });
+  it('Should validate filtering from now to 6 months', async () => {
+    const res = await request(baseUrl)
+      .post(endpoint)
+      .send({
+        filters: {
+          status: [],
+          groups: [],
+          recency: ["<6"]
+        }
+      });
 
-  //   // 1. Validate 200 response code
-  //   expect(res.status).to.equal(200);
+    expect(res.status).to.equal(200);
 
-  //   // 2. Validate all recency values are <6 months
-  //   res.body.data.forEach(expert => {
-  //     const monthsSinceLastWorked = expert.recency;
-  //     expect(monthsSinceLastWorked).to.be.lessThan(6);
-  //   });
-  // });
-
-  // describe('GET /character/46', () => {
-//   it('should return the character Bill', async () => {
-//     try {
-//       const res = await request(baseUrlRealApi).get('/api/character/46');
-      
-//       // Validações
-//       expect(res).to.have.status(200);
-//       expect(res.body).to.be.an('object');
-//       expect(res.body.name).to.equal('Bill');
-//     } catch (error) {
-//       console.error('Erro na requisição:', error);
-//       throw error; // Repassa o erro para garantir que o teste falhe corretamente
-//     }
-//   });
-// });
+    res.body.data.forEach(expert => {
+      const monthsSinceLastWorked = expert.recency;
+      expect(monthsSinceLastWorked).to.be.lessThan(6);
+    });
+  });
 });
